2f9d8c1d18b7071fcc04896a0b10c815e892b48d
ebabnik <e.babnik@bellevuecollelge.edu>
2022-11-05 20:20:08 -0700

    move up upleft

--- artifact03/rhombus.js
+++ artifact03/rhombus.js
@@ -9,6 +9,29 @@ function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
     
     }
     
+    function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+        var rLine ="";
+        for (i=0;i<pHeight;i++){
+        rLine +="<p>";
+        //Create each line on the Rhombus
+        for(j=0;j<=i;j++){
+        
+        //Is the position even or odd so we change the color
+        if (j%2)
+        //even
+        rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+        else
+        //odd
+        rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+        
+        }
+        rLine +="</p>";
+        // console.log(rLine);
+        
+        }
+        
+        document.getElementById("upLeft").innerHTML = rLine;
+
     function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
     var rLine ="";
     for (i=0;i<pHeight;i++){
@@ -57,27 +80,6 @@ function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
     document.getElementById("downRight").innerHTML = rLine;
     }
 
-    function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
-        var rLine ="";
-        for (i=0;i<pHeight;i++){
-        rLine +="<p>";
-        //Create each line on the Rhombus
-        for(j=0;j<=i;j++){
-        
-        //Is the position even or odd so we change the color
-        if (j%2)
-        //even
-        rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-        else
-        //odd
-        rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
-        
-        }
-        rLine +="</p>";
-        // console.log(rLine);
-        
-        }
-        
-        document.getElementById("upLeft").innerHTML = rLine;
+
         }
 
