4469ac796cf83e36730e461f9edbac22c40e5f67
ebabnik <e.babnik@bellevuecollelge.edu>
2022-11-05 21:44:27 -0700

    paste code

--- artifact03/rhombus.js
+++ artifact03/rhombus.js
@@ -4,9 +4,42 @@
 // var symbol ="*";
 
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
+    upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
     upRight(pHeight, pColorEven, pColorOdd, pSymbol);
+    downLeft(pHeight, pColorEven, pColorOdd, pSymbol);  
     downRight(pHeight, pColorEven, pColorOdd, pSymbol);
     
+    }
+    
+    function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+    var rLine ="";
+    for (i=1;i<=pHeight;i++){
+    rLine +="<p>";
+    
+    // Fill in the left side of every row with spaces or blanks
+      for(x=1;x<=pHeight-i;x++){
+        //rLine +="-";
+       rLine +="<span class='space'>" + pSymbol +"</span>";
+      }
+      
+    //Create each line on the Rhombus
+    for(j=x;j<=pHeight;j++){
+    
+    //Is the position even or odd so we change the color
+    if (j%2)
+    //even
+    rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+    else
+    //odd
+    rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+    
+    }
+    rLine +="</p>";
+    // console.log(rLine);
+    
+    }
+    
+    document.getElementById("upLeft").innerHTML = rLine;
     }
     
     function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
@@ -33,6 +66,38 @@ function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
     document.getElementById("upRight").innerHTML = rLine;
     }
     
+    
+    function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+    var rLine ="";
+    for (i=pHeight;i > 0;i--){
+    rLine +="<p>";
+    
+    // Fill in the left side of every row with spaces or blanks
+      for(x=1;x<=pHeight-i;x++){
+        //rLine +="-";
+       rLine +="<span class='space'>" + pSymbol +"</span>";
+      }  
+      
+    //Create each line on the Rhombus
+    for(j=0;j<i;j++){
+    
+    //Is the position even or odd so we change the color
+    if (j%2)
+    //even
+    rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+    else
+    //odd
+    rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+    
+    }
+    rLine +="</p>";
+    // console.log(rLine);
+    
+    }
+    
+    document.getElementById("downLeft").innerHTML = rLine;
+    }
+    
     function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
     var rLine ="";
     for (i=pHeight;i > 0;i--){
@@ -56,5 +121,3 @@ function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
     
     document.getElementById("downRight").innerHTML = rLine;
     }
-    
-     
