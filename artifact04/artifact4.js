8d75e8004de2287f89d9714610f530d886411c5d
ebabnik <e.babnik@bellevuecollelge.edu>
2022-11-10 20:02:41 -0800

    update code

--- artifact04/artifact4.js
+++ artifact04/artifact4.js
@@ -1,4 +1,4 @@
-function isValid() {
+function validateForm() {
     if (firstName() //&&
        // lastName()
     )
@@ -8,14 +8,21 @@ function isValid() {
         event.preventDefault();
         return false;
 }
-
+ 
 FirstName.addEventListener('blur', firstName, false);
 function firstName(){
     //1) Create variable
     var validFirstname=false;
+    var validLastname=false;
+    var validEmail=false;
+  
 
     //2) read value from HTML
     var firstname = document.getElementById("FirstName").value;
+    var lastname = document.getElementById("LastName").value;
+    var userEmail = document.getElementById("Email").value;
+    var atpos = userEmail.indexOf("@");
+    var dotpos = userEmail.lastIndexOf(".");
     var errorMessages = "";
 
     //3) Do validation
@@ -23,16 +30,36 @@ function firstName(){
         errorMessages += "<p>The first name is required and cannot be greater than 20 characters</p>";
         console.log("First name invalid — length")
         } else if (firstname.match("^[a-zA-Z ,.'-]+$")===null) {
-            errorMessages += "<p>Invalid caracter in last name (accepts only A-Z, a-z, and ,.'-)</p>";
+            errorMessages += "<p>Invalid caracter in first name (accepts only A-Z, a-z, and ,.'-)</p>";
             console.log("First name invalid — bad characters")
         } else {
                 validFirstname = true;
                 console.log("First name valid")
         };
-
+  
+    if (lastname==="null" || lastname==="" || lastname.length > 50 ) {
+        errorMessages += "<p>The last name is required and cannot be greater than 20 characters</p>";
+        console.log("Last name invalid — length")
+        } else if (lastname.match("^[a-zA-Z ,.'-]+$")===null) {
+            errorMessages += "<p>Invalid caracter in last name (accepts only A-Z, a-z, and ,.'-)</p>";
+            console.log("Last name invalid — bad characters")
+        } else {
+                validLastname = true;
+                console.log("Last name valid")
+        };  
+  
+    if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=userEmail.length) {
+        errorMessages += "<p>Invalid email</p>";
+        console.log("email invalid")
+        } else {
+                validEmail = true;
+                console.log("Email valid")
+        };  
+    
     //4) Send error message to HTML
     document.getElementById("fname").innerHTML = errorMessages;
+    
 
     //5) return status of each field
-    return (validFirstname);
+    return (validFirstname && validLastname && validEmail);
 };
